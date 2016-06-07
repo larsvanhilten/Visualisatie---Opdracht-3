e9009e8193189f468eb66f460f50353878bfef4b
Lars van Hilten <larsvh8@gmail.com>
2016-06-07 13:38:18 +0200

    0.1.1

--- Visualisatie---Opdracht-3/src/main/java/com/mycompany/visualisatie/opdracht/Database.java
+++ Visualisatie---Opdracht-3/src/main/java/com/mycompany/visualisatie/opdracht/Database.java
@@ -4,18 +4,25 @@
  * and open the template in the editor.
  */
 package com.mycompany.visualisatie.opdracht;
+
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author Lars
  */
 public class Database {
-    
+
     Connection connection;
-    
-    public Database(){
+
+    public Database() {
+        createDatabase();
+        createTable();
+    }
+
+    private void createDatabase() {
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:coordinates.db");
@@ -24,7 +31,25 @@ public class Database {
         } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
+
     }
-    
+
+    private void createTable(String tablename) {
+
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "CREATE TABLE ROTTERAM_WEST"
+                    + "(ID INT PRIMARY KEY     NOT NULL,"
+                    + " X           DOUBLE    NOT NULL, "
+                    + " Y            DOUBLE     NOT NULL, "
+                    + " Z        DOUBLE     NOT NULL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
